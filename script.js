5f32abef9be13d185106281431ee1b7c49b8de44
FelipeC-05 <barbosacarvalhodoamaralfelipe@gmail.com>
2023-11-30 06:24:00 -0300

    teste

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+ document.addEventListener("DOMContentLoaded", function () {
   var menuItems = document.querySelectorAll('.menu-item');
   var contents = document.querySelectorAll('.content');
 
